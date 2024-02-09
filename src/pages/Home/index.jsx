5efc55f8e979aef8978645694149c81ada6ae7ab
Cesar Augusto <gutoamorim@outlook.com>
2024-02-09 02:46:57 -0300

    Page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,23 @@
+import styles from "./home.module.css";
+import pizzaIllustration from "../../assets/pizza-illustration.png";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className={styles.home}>
+      <div className={styles.left}>
+        <h1>Pizzas 10% OFF</h1>
+        <h2>Confira nosso cardápio</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          vitae beatae sint magnam, libero harum quae nobis veritatis iure hic
+          provident illo porro.
+        </p>
+        <Link className="button">Fazer pedido</Link>
+      </div>
+      <div className={styles.image}>
+        <img src={pizzaIllustration} alt="Ilustração de uma pizza" />
+      </div>
+    </div>
   );
 }
